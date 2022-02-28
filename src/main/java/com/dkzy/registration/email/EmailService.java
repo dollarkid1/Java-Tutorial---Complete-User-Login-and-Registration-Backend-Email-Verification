5dfed6a6e5811f2ecdddbb7e777a0dcb8f7b1c52
Humble Douglas <dollarkid17@gmail.com>
2022-02-28 02:07:51 +0100

    new and final commit

--- src/main/java/com/dkzy/registration/email/EmailService.java
+++ src/main/java/com/dkzy/registration/email/EmailService.java
@@ -3,9 +3,12 @@ package com.dkzy.registration.email;
 import lombok.AllArgsConstructor;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import javax.mail.MessagingException;
@@ -13,14 +16,15 @@ import javax.mail.internet.MimeMessage;
 
 //import java.util.logging.Logger;
 
+
 @Service
-@AllArgsConstructor
 public class EmailService implements EmailSender{
 
 
     private final static Logger LOGGER = LoggerFactory.getLogger(EmailService.class);
 
-    private final JavaMailSender mailSender;
+
+    private JavaMailSender mailSender;
 
 
     @Override
